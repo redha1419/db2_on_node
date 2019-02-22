e67b979f7127b2b14e461df4b973552228e5d8fd
Reza Shahriari <reza_shahriari@yahoo.ca>
2019-02-21 22:04:39 -0500

    added route and env file

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+
+//passing routes
+const db2Routes = require('./server/routes/db2_connect');
+
 require('dotenv').config();
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
-})
+});
+
+
+app.use('/db2', db2Routes);
 
 app.listen(process.env.DEV_PORT, function() {
   console.info("==> 🌎 Peep port %s.", process.env.DEV_PORT);
---
+++ server/routes/db2_connect.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = new express.Router();
+const db2 = require('ibm_db');
+require('dotenv').config();
+const ibmStr = "DATABASE=" + process.env.DB_NAME + ";HOSTNAME=" + process.env.HOSTNAME + ";PORT=" + process.env.PORT+ ";PROTOCOL=" + process.env.PROTOCAL + ";UID=" + process.env.UID + ";PWD=" + process.env.PWD;
+
+
+// for now we have our dummy post, just a connect to db and a quick select statement
+router.post('/connect', function(req, res) {	
+  db2.open(ibmStr, function (err, connection) {
+    if (err){
+      console.log(err);
+      return;
+    }
+    connection.query("select 1 from sysibm.sysdummy1", function (err1, rows) {
+      if (err1){
+        console.log(err1);
+      }
+      else{
+        //console.log(rows); for debugging
+        res.json({result: rows});
+        res.status(200);
+      }
+      connection.close(function(err2) {
+        if(err2){
+	        console.log(err2);
+      	}
+      });
+    });
+  });
+});
+
+module.exports = router;
